b5c3accc086774af9d78e97017fa54af6f34aee4
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-07-16 14:49:35 +0700

    delete personal access tokens

--- app/Models/PackagePrice.php
+++ app/Models/PackagePrice.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class PackagePrice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'duration_months',
+        'description',
+    ];
+
+    public $timestamps = false;
 }
