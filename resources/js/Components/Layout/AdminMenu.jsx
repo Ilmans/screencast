4aa65af0f525e6e7f0a2f4c4c7c967b703198251
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-07-30 23:56:19 +0700

    responsive dashboard admin

--- resources/js/Components/Layout/AdminMenu.jsx
+++ resources/js/Components/Layout/AdminMenu.jsx
@@ -1,93 +1,94 @@
-import React from 'react'
-import VerticalNavLink from '../VerticalNavLink'
-import { cn } from '@/lib/utils'
+import React, { Fragment } from "react";
+import VerticalNavLink from "../VerticalNavLink";
+import { cn } from "@/lib/utils";
+import { IconMenu2 } from "@tabler/icons-react";
 
-function AdminMenu({dashboard = true}) {
-  return (
-    <ul
-    className={cn(
-        "flex flex-col ",
-        dashboard ? "text-sm gap-y-2" : "gap-y-1 text-xs mb-2"
-    )}
->
-    {/* only guest can access this menu, this menu show in nav mobile */}
+function AdminMenu({ open = false }) {
+    return (
+        <Fragment>
+            <ul
+                className={cn(
+                    !open ? "translate-y-full duration-300 h-0 opacity-0 transition-all  md:p-0 md:translate-y-0 md:opacity-100 " : "translate-y-0 duration-300 opacity-100 transition-all p-4 md:p-0",
+                    `text-sm gap-y-2 gap-x-2 space-y-2 mb-2 bg-muted md:bg-background  md:p-0 rounded-lg md:rounded-none`
+                )}
+            >
+                {/* only guest can access this menu, this menu show in nav mobile */}
 
-    {/* home */}
-   
-        <VerticalNavLink
-            href="/admin/dashboard"
-            icon="IconHomeEco"
-            active={route().current("admin.dashboard")}
-        >
-            Dashboard
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/topics"
-            icon="IconCategory2"
-            active={route().current("admin.topics.index")}
-        >
-            Topik
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/series"
-            icon="IconBrandMeta"
-            active={route().current("admin.series.index")}
-        >
-            Series
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/articles"
-            icon="IconBook"
-            active={route().current("admin.series.index")}
-        >
-            Artikel
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/subscriptions"
-            icon="IconCoin"
-            active={route().current("admin.subscriptions.index")}
-        >
-            Langganan
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/package-prices"
-            icon="IconReceipt2"
-            active={route().current("admin.package_prices.index")}
-        >
-            Daftar Harga
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/invoices"
-            icon="IconFileDollar"
-            active={route().current("admin.invoices.index")}
-        >
-            Invoices
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/payment_methods"
-            icon="IconCreditCard"
-            active={route().current("admin.payment_methods.index")}
-        >
-            Metode Pembayaran
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/users"
-            icon="IconUsers"
-            active={route().current("admin.users.index")}
-        >
-            Users
-        </VerticalNavLink>
-        <VerticalNavLink
-            href="/admin/settings"
-            icon="IconSettingsBolt"
-            active={route().current("admin.settings.index")}
-        >
-            Website Setting
-        </VerticalNavLink>
+                {/* home */}
 
-       
-        </ul>
-  )
+                <VerticalNavLink
+                    href="/admin/dashboard"
+                    icon="IconHomeEco"
+                    active={route().current("admin.dashboard")}
+                >
+                    Dashboard
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/topics"
+                    icon="IconCategory2"
+                    active={route().current("admin.topics.index")}
+                >
+                    Topik
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/series"
+                    icon="IconBrandMeta"
+                    active={route().current("admin.series.index")}
+                >
+                    Series
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/articles"
+                    icon="IconBook"
+                    active={route().current("admin.series.index")}
+                >
+                    Artikel
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/subscriptions"
+                    icon="IconCoin"
+                    active={route().current("admin.subscriptions.index")}
+                >
+                    Langganan
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/package-prices"
+                    icon="IconReceipt2"
+                    active={route().current("admin.package_prices.index")}
+                >
+                    Daftar Harga
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/invoices"
+                    icon="IconFileDollar"
+                    active={route().current("admin.invoices.index")}
+                >
+                    Invoices
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/payment_methods"
+                    icon="IconCreditCard"
+                    active={route().current("admin.payment_methods.index")}
+                >
+                    Metode Pembayaran
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/users"
+                    icon="IconUsers"
+                    active={route().current("admin.users.index")}
+                >
+                    Users
+                </VerticalNavLink>
+                <VerticalNavLink
+                    href="/admin/settings"
+                    icon="IconSettingsBolt"
+                    active={route().current("admin.settings.index")}
+                >
+                    Website Setting
+                </VerticalNavLink>
+            </ul>
+        </Fragment>
+    );
 }
 
-export default AdminMenu
+export default AdminMenu;
--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import Header from './Header'
-import AdminMenu from '@/Components/Layout/AdminMenu'
-import { cn } from '@/lib/utils'
+import React from "react";
+import Header from "./Header";
+import AdminMenu from "@/Components/Layout/AdminMenu";
+import { cn } from "@/lib/utils";
+import { IconMenu2 } from "@tabler/icons-react";
 
-function AdminLayout({header= true,children,needFull=false}) {
-  return (
-    <div className={` `}>
-    {header && <Header />}
-    <div className="py-8 md:container md:grid md:grid-cols-5 md:mt-8 gap-x-4">
-        <div className="hidden col-span-1 rounded-lg md:block h-fit">
-            <AdminMenu />
+function AdminLayout({ header = true, children, needFull = false }) {
+    const [openMobileMenu, setOpenMobileMenu] = React.useState(false);
+    return (
+        <div className={` `}>
+            {header && <Header />}
+            <div className="py-8 md:container md:grid md:grid-cols-5 md:mt-8 gap-x-4">
+                <div className="col-span-1 rounded-lg h-fit">
+                    <button
+                        onClick={() => setOpenMobileMenu(!openMobileMenu)}
+                    className="flex items-center justify-center w-full py-2 mb-2 text-xs rounded-lg gap-x-2 hover:bg-accent lg:hidden bg-muted">
+                        <IconMenu2 className="w-4 h-4" />
+                        <span>Admin menu</span>
+                    </button>
+                    <AdminMenu
+                        open={openMobileMenu}
+                    />
+                </div>
+                <div
+                    className={cn(
+                        needFull ? "md:col-span-4" : "md:col-span-3",
+                        "h-fit rounded-lg"
+                    )}
+                >
+                    {children}
+                </div>
+            </div>
         </div>
-        <div
-            className={cn(
-                needFull ? "md:col-span-4" : "md:col-span-3",
-                "h-fit rounded-lg"
-            )}
-        >
-            {children}
-        </div>
-    </div>
-</div>
-  )
+    );
 }
 
-export default AdminLayout
+export default AdminLayout;
