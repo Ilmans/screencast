cb0edb33e01668848a2babbfd0b17e7c03704dd2
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-11-14 09:07:35 +0700

    use native css in error page render

--- resources/js/Pages/Error.jsx
+++ resources/js/Pages/Error.jsx
@@ -1,25 +1,38 @@
-export default function ErrorPage({ status }) {
-    console.log(status);
-    const title = {
-        503: "503: Service Unavailable",
-        500: "500: Server Error",
-        404: "404: Page Not Found",
-        403: "403: Forbidden",
-    }[status];
+import React from 'react';
 
-    const description = {
-        503: "Sorry, we are doing some maintenance. Please check back soon.",
-        500: "Whoops, something went wrong on our servers.",
-        404: "Sorry, the page you are looking for could not be found.",
-        403: "Sorry, you are forbidden from accessing this page.",
-    }[status];
+const ErrorPage = ({ status }) => {
+  const title = {
+    503: "503: Service Unavailable",
+    500: "500: Server Error",
+    404: "404: Page Not Found",
+    403: "403: Forbidden",
+  }[status];
 
-    return (
-        <div className="flex items-center justify-center h-screen text-white">
-            <div>
-                <h1 className="text-lg font-semibold">{title}</h1>
-                <div>{description}</div>
-            </div>
-        </div>
-    );
-}
+  const description = {
+    503: "Sorry, we are doing some maintenance. Please check back soon.",
+    500: "Whoops, something went wrong on our servers.",
+    404: "Sorry, the page you are looking for could not be found.",
+    403: "Sorry, you are forbidden from accessing this page.",
+  }[status];
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', color: '#fff' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ fontSize: '24px', marginBottom: '10px' }}>{title}</h1>
+        <div style={{ fontSize: '18px' }}>{description}</div>
+      </div>
+      <style>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+            background-color: #333;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default ErrorPage;
