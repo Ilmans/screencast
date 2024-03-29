ab8ef2fa4357e541e361b841341960977f5912e6
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2024-03-30 03:54:39 +0700

    fix import

--- resources/js/Components/ui/toaster.jsx
+++ resources/js/Components/ui/toaster.jsx
@@ -1,51 +1,60 @@
 import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+    Toast,
+    ToastClose,
+    ToastDescription,
+    ToastProvider,
+    ToastTitle,
+    ToastViewport,
+} from "@/components/ui/toast";
+
 import { usePage } from "@inertiajs/react";
 import { useEffect } from "react";
+import { useToast } from "./use-toast";
 
 export function Toaster() {
-  const { toasts,toast } = useToast()
-  const {success,error} = usePage().props
+    const { toasts, toast } = useToast();
+    const { success, error } = usePage().props;
 
-  useEffect(() => {
-    if(success){
-      toast({
-        title: "Success",
-        description: success,
-      })
-    } else if(error){
-      toast({
-        title: "Error",
-        description: error,
-        variant: "destructive",
-      })
-    }
-  }, [success,error])
+    useEffect(() => {
+        if (success) {
+            toast({
+                title: "Success",
+                description: success,
+            });
+        } else if (error) {
+            toast({
+                title: "Error",
+                description: error,
+                variant: "destructive",
+            });
+        }
+    }, [success, error]);
 
-  return (
-    (<ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          (<Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>)
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>)
-  );
+    return (
+        <ToastProvider>
+            {toasts.map(function ({
+                id,
+                title,
+                description,
+                action,
+                ...props
+            }) {
+                return (
+                    <Toast key={id} {...props}>
+                        <div className="grid gap-1">
+                            {title && <ToastTitle>{title}</ToastTitle>}
+                            {description && (
+                                <ToastDescription>
+                                    {description}
+                                </ToastDescription>
+                            )}
+                        </div>
+                        {action}
+                        <ToastClose />
+                    </Toast>
+                );
+            })}
+            <ToastViewport />
+        </ToastProvider>
+    );
 }
